2766e74bafc54a6ec1bc25c120fe9bf7d41d3f18
RomainBonnetKKRT <romain.bonnet@coda-student.school>
2026-01-08 11:36:51 +0100

    ajouter le fichier html + partie une du tp

---
+++ script.js
@@ -0,0 +1,9 @@
+//Partie une//
+
+const nom_classe = "B1A";
+let nombre_eleves = 3;
+let classe_ouverte = true;
+
+console.log("Nom de la classe :", nom_classe);
+console.log("Nombre d'élèves :", nombre_eleves);
+console.log("Classe ouverte :", classe_ouverte);
