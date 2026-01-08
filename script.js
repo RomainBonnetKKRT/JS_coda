cd433563ff80ba1b56be497118993d92ea0f4475
RomainBonnetKKRT <romain.bonnet@coda-student.school>
2026-01-08 11:47:43 +0100

    partie_2

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ let classe_ouverte = true;
 console.log("Nom de la classe :", nom_classe);
 console.log("Nombre d'élèves :", nombre_eleves);
 console.log("Classe ouverte :", classe_ouverte);
+
+//Partie deux//
+
+let eleve_1 = {
+  prenom: "Romain",
+  note_maths: 20,
+  note_francais: 18
+};
+
+console.log("Prénom de l'élève :", eleve_1.prenom);
+
+//
